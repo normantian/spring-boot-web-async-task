ca4cbd995c50c753e243122d61e5282294cbebf0
田飞 <tianfei@wusongtech.com>
2018-10-15 10:27:26 +0800

    simple limiter

---
+++ src/test/java/com/norman/guava/CacheTest.java
@@ -0,0 +1,81 @@
+package com.norman.guava;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author tianfei
+ * @version 1.0.0
+ * @description </br>
+ * @date 2018/10/9 上午9:17.
+ */
+public class CacheTest {
+
+    static class UserService {
+
+        private final Cache<String, String> cache;
+
+        public UserService() {
+            /**
+             * 5秒自动过期
+             */
+            cache = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.SECONDS).build();
+
+        }
+
+        public String getUserName(String id) throws Exception {
+            return cache.get(id, () -> {
+                System.out.println("method invoke2");
+                //这里执行查询数据库，等其他复杂的逻辑
+                return "User:" + id;
+            });
+        }
+
+        public static void main(String[] args) throws Exception {
+            UserService us = new UserService();
+            for (int i = 0; i < 20; i++) {
+                System.out.println(us.getUserName("1001"));
+                TimeUnit.SECONDS.sleep(1);
+            }
+        }
+
+    }
+
+    static class UserService2 {
+
+        private final LoadingCache<String, String> cache;
+
+        public UserService2() {
+            /*** 5秒自动过期		 */
+            cache = CacheBuilder.newBuilder()
+                    .expireAfterWrite(5, TimeUnit.SECONDS)
+                    .build(new CacheLoader<String, String>() {
+
+                public String load(String id) throws Exception {
+                    System.out.println("method inovke");
+                    //这里执行查询数据库，等其他复杂的逻辑
+                    return "User:" + id;
+                }
+            });
+
+        }
+
+        public String getUserName(String id) throws Exception {
+            return cache.get(id);
+        }
+
+        public static void main(String[] args) throws Exception {
+            UserService2 us = new UserService2();
+            for (int i = 0; i < 20; i++) {
+                System.out.println(us.getUserName("1001"));
+                TimeUnit.SECONDS.sleep(1);
+            }
+        }
+
+    }
+
+}
--- src/test/java/com/norman/redis/SimpleRateLimiter.java
+++ src/test/java/com/norman/redis/SimpleRateLimiter.java
@@ -4,15 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -29,61 +25,63 @@ public class SimpleRateLimiter {
     StringRedisTemplate stringRedisTemplate;
 
     @Test
-    public void test() throws Exception{
+    public void test() throws Exception {
         // 一分钟内只允许最多回复 5 个帖子
-        for(int i=0;i<20;i++) {
-            System.out.println(isActionAllowed("laoqian", "reply", 10, 5));
-            TimeUnit.SECONDS.sleep(1);
-        }
+        for (int i = 0; i < 20; i++) {
+            System.out.println(isActionAllowed("laoqian", "reply", 15, 5));
+            if(i%5 == 0){
+                TimeUnit.SECONDS.sleep(10);
+            } else {
+                TimeUnit.SECONDS.sleep(3);
+            }
 
-        TimeUnit.SECONDS.sleep(20);
-        System.out.println(isActionAllowed("laoqian", "reply", 20, 5));
+        }
     }
 
-    public boolean isActionAllowed(String userId, String actionKey, int period, int maxCount){
+    public boolean isActionAllowed(String userId, String actionKey, int period, int maxCount) {
         String key = String.format("hist:%s:%s", userId, actionKey);
 
         final List<Object> objects = stringRedisTemplate.executePipelined(
-                new RedisCallback<Object>() {
-                    @Override
-                    public Object doInRedis(RedisConnection connection) throws DataAccessException {
-                        connection.openPipeline();
-                        boolean isPipelined = connection.isPipelined();
+//                new RedisCallback<Object>() {
+//                    @Override
+//                    public Object doInRedis(RedisConnection connection) throws DataAccessException {
+                (RedisCallback<List<Object>>) connection -> {
+                    connection.openPipeline();
+                    boolean isPipelined = connection.isPipelined();
 
-                        try {
-                            if (isPipelined) {
+                    try {
+                        if (isPipelined) {
 //                                final ZSetOperations<String, String> zsetOps = stringRedisTemplate.opsForZSet();
 
-                                long nowTs = System.currentTimeMillis();
-                                // value 和 score 都使用毫秒时间戳
+                            long nowTs = System.currentTimeMillis();
+                            // value 和 score 都使用毫秒时间戳
 //                                zsetOps.add(key, "" + nowTs, nowTs);
-                                connection.zAdd(key.getBytes(), nowTs, (nowTs+"").getBytes());
+                            connection.zAdd(key.getBytes(), nowTs, (nowTs + "").getBytes());
 
-                                //移除时间窗口之前的行为记录，剩下的都是时间窗口内的
-                                connection.zRemRangeByScore(key.getBytes(), 0 , nowTs - period * 1000);
+                            //移除时间窗口之前的行为记录，剩下的都是时间窗口内的
+                            connection.zRemRangeByScore(key.getBytes(), 0, nowTs - period * 1000);
 //                                zsetOps.removeRangeByScore(key, 0, nowTs - period * 1000);
-                                // 移除时间窗口之前的行为记录，剩下的都是时间窗口内的
+                            // 移除时间窗口之前的行为记录，剩下的都是时间窗口内的
 //                                count = zsetOps.zCard(key);
-                                connection.zCard(key.getBytes());
+                            connection.zCard(key.getBytes());
 
-                                //设置 zset 过期时间，避免冷用户持续占用内存
-                                //过期时间应该等于时间窗口的长度，再多宽限 1s
-                                connection.expire(key.getBytes(), period + 1);
+                            //设置 zset 过期时间，避免冷用户持续占用内存
+                            //过期时间应该等于时间窗口的长度，再多宽限 1s
+                            connection.expire(key.getBytes(), period + 1);
 
 
-                            }
+                        }
 
-                        } catch (Exception e) {
+                    } catch (Exception e) {
 
-                        } finally {
+                    } finally {
 //                            connection.exec();
 //                            connection.closePipeline();
-                        }
-                        return null;
                     }
+                    return null;
                 });
         System.out.println(objects);
-        return (Long)(objects.get(2)) <= maxCount;
+        return (Long) (objects.get(2)) <= maxCount;
     }
 
 //    public boolean isActionAllowed(String userId, String actionKey, int period, int maxCount) {
